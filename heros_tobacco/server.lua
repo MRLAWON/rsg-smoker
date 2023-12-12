dfa26492cf9ff099b5babccaf98ef053139bc946
MRLAWON <89027587+MRLAWON@users.noreply.github.com>
2023-12-12 13:42:13 +0000

    Update server.lua

--- heros_tobacco/server.lua
+++ heros_tobacco/server.lua
@@ -1,21 +1,26 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
 RSGCore.Functions.CreateUseableItem("cigar", function(source, item)
-		TriggerClientEvent('prop:cigar', source)
-		TriggerClientEvent("vorp:TipRight", source, Config.Text.Cigar, 3000)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('prop:cigar', source)
+    end
 end)
 
 RSGCore.Functions.CreateUseableItem("chewingtobacco", function(source, item)
-	TriggerClientEvent('prop:chewingtobacco', source)
-	TriggerClientEvent("vorp:TipRight", source, Config.Text.TextA, 3000)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('prop:chewingtobacco', source)
+    end
 end)
 
 RSGCore.Functions.CreateUseableItem("cigarette", function(source, item)
-	TriggerClientEvent('prop:cigaret', source)
-	TriggerClientEvent("vorp:TipRight", source, Config.Text.cigarette, 3000)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('prop:cigaret', source)
+    end
 end)
 
 RSGCore.Functions.CreateUseableItem("pipe_smoker", function(source, item)
 		TriggerClientEvent('prop:pipe_smoker', source)
-		TriggerClientEvent("vorp:TipRight", source, Config.Text.Pipe, 3000)
 end)
